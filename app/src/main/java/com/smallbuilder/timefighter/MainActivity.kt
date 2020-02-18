34874a79495b06062bd5266d09e1af6aff0f637b
AME <adrien.mechin@gmail.com>
2020-02-18 09:54:54 +0100

    about menu added

--- app/src/main/java/com/smallbuilder/timefighter/MainActivity.kt
+++ app/src/main/java/com/smallbuilder/timefighter/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.smallbuilder.timefighter
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
-import android.view.animation.AlphaAnimation
-import android.view.animation.Animation
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -58,7 +60,31 @@ class MainActivity : AppCompatActivity() {
         } else{
             resetGame()
         }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.actionAbout){
+            showInfo()
+        }
+        return true
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    private fun showInfo(){
+        val dialogTitle = getString(R.string.aboutTitle, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.aboutMessage)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+
+        builder.create().show()
     }
 
     private fun restoreGame() {
